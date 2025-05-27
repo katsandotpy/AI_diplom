6c310ba17e0f30fb4adfbd65dfe6b3bca988ab7b
spazzbk <glino.money.wx@gmail.com>
2025-05-27 14:50:01 +0300

    Initial commit

--- AI_tgbot.py
+++ AI_tgbot.py
@@ -1,6 +1,23 @@
 import telebot
+from googletrans import Translator
+import asyncio
+import json
+from transformers import AutoModelForCausalLM, AutoTokenizer
 
-bot = telebot.TeleBot('7229031292:AAFNoSpyzVMAk5f6uMSbB1wkA3GZy5od0II')
+
+
+
+model = AutoModelForCausalLM.from_pretrained("/model")
+tokenizer = AutoTokenizer.from_pretrained("/model")
+
+
+with open("config.json", "r", encodinf="utf-8") as file:
+    config = json.load(file)
+
+
+API_KEY = config.get("API_KEY")
+
+bot = telebot.TeleBot(API_KEY)
 
 @bot.message_handler(commands=['start'])
 def main(message):
@@ -8,7 +25,23 @@ def main(message):
 
 @bot.message_handler()
 def z(message):
-    bot.send_message(message.chat.id, message.text)
+    input_text = asyncio.run(RU_EN(message.text))
+    inputs = tokenizer(input_text, return_tensors="pt").to("cuda")
+    outputs = model.generate(**inputs)
+    answer = asyncio.run(EN_RU(tokenizer.decode(outputs[0], skip_special_tokens=True)))
+    bot.send_message(message.chat.id, answer)
+
+
+translator = Translator()
+async def EN_RU(message):
+    async with Translator() as translator:
+        r = await translator.translate(message, src='en', dest='ru')
+        return r.text
+
+async def RU_EN(message):
+    async with Translator() as translator:
+        r = await translator.translate(message, src='ru', dest='en')
+        return r.text
 
 
 bot.polling(none_stop=True)
